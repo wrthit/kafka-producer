135044bcdf67fb55b36edd214357b13ad3b2169e
Joseph Hicks <jthicks@gmail.com>
2021-02-11 08:33:27 -0600

    adding @Configuration fixes module error

--- src/main/java/com/kafka/example/producer/service/NotificationService.java
+++ src/main/java/com/kafka/example/producer/service/NotificationService.java
@@ -1,7 +1,5 @@
 package com.kafka.example.producer.service;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.kafka.example.producer.model.Notification;
 
 import org.springframework.kafka.core.KafkaTemplate;
@@ -19,12 +17,5 @@ public class NotificationService {
 
     public void logNotification(Notification notification) {
         kafkaTemplate.send(TOPIC, notification);
-        // String notificationAsString;
-        // try {
-        //     notificationAsString = new ObjectMapper().writeValueAsString(notification);
-        //     kafkaTemplate.send(TOPIC, notificationAsString);
-        // } catch (JsonProcessingException e) {
-        //     e.printStackTrace();
-        // }
     }
 }
